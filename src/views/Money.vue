68a00cd35d6dfc1f756814b30418445649383a3b
wekbor <364102504@qq.com>
2020-08-24 22:53:51 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,28 @@
 import Money from '*@/views/Money.vue';
 <template>
-    <div>Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            Money.vue
+        </div>
         <Nav/>
     </div>
 </template>
 
-<script>
+<script lang="ts">
     export default {
-        name: 'money',
+        name: 'Money',
     }
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+      border: 1px solid blue;
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+  }
+  .content{
+      overflow: auto;
+      flex-grow: 1;
+  }
 </style>
