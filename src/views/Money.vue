fdc116a8ed6a95a6424237287fe62310ec7bd2a2
wekbor <364102504@qq.com>
2020-08-26 22:02:50 +0800

    添加全局样式和变量

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,20 +1,56 @@
 import Money from '*@/views/Money.vue';
 import Layout from '@/components/Layout.vue';
 <template>
-     <Layout>
-         <p>Money.vue</p>
-     </Layout>
+  <Layout>
+    <div class="tags">
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+          <span class="name">备注</span>
+          <input type="text">
+      </label>
+    </div>
+    <div>
+        <ul class="types">
+            <li class="selected">支出</li>
+            <li>收入</li>
+        </ul>
+    </div>
+    <div class="numberPad">
+        <div class="output">100</div>
+        <div buttons>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>删除</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>清空</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>ok</button>
+            <button>0</button>
+            <button>.</button>
+        </div>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Money',
-    }
+export default {
+  name: "Money",
+};
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-    p{
-        color: $red;
-    }
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-eslint-disable 
-const path = require('path')
+eslint-disable
+const path = require('path')  
 
 module.exports = {
   lintOnSave: false,
