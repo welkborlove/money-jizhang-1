c3fe35e365c456c667b9a7e4aba6940abee67c65
wekbor <364102504@qq.com>
2020-09-02 22:30:43 +0800

    添加点击标签可跳转页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,12 +2,12 @@ import Nav from '@/components/Nav.vue';
 
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <icon name="right" />
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -27,10 +27,10 @@ export default class Labels extends Vue {
     const name = window.prompt("请输入标签名");
     if (name) {
       const message = tagListModel.create(name);
-      if(message === 'duplicated'){
-          window.alert('标签名重复');
-      } else if(message === 'success'){
-          window.alert('添加成功');
+      if (message === "duplicated") {
+        window.alert("标签名重复");
+      } else if (message === "success") {
+        window.alert("添加成功");
       }
     }
   }
@@ -42,7 +42,7 @@ export default class Labels extends Vue {
   background: white;
   font-size: 14px;
   padding-left: 16px;
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
