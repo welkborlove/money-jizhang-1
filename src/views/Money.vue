55adaad43411375ab6201bf1f46aa9ab41b3b46c
wekbor <364102504@qq.com>
2020-09-09 22:56:37 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,14 +15,13 @@
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Tags from "@/components/Money/Tags.vue";
-import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import { Component } from "vue-property-decorator";
 import recordTypeList from "@/constants/recordTypeList";
 import Tabs from '@/components/Tabs.vue';
 
 @Component({
-  components: {Tabs ,Tags, Notes, Types, NumberPad },
+  components: {Tabs ,Tags, Notes, NumberPad },
 })
 export default class Money extends Vue {
   get recordList() {
