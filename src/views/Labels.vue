2e639bfc0b5c325022da3d99e4574efe174bf21d
wekbor <364102504@qq.com>
2020-09-09 10:52:42 +0800

    Labels.vue 使用 Vuex

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,20 +18,22 @@ import Nav from '@/components/Nav.vue';
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Button from '@/components/Button.vue';
+import {mixins} from 'vue-class-component'
+import TagHelper from '@/mixins/TagHelper';
 
 
 @Component({
-    components: {Button}
-})
-export default class Labels extends Vue {
-  tags = [];
-  createTag() {
-    const name = window.prompt("请输入标签名");
-    if(name){
-      
-       // store.createTag(name);
+    components: {Button},
+    computed: {
+      tags(){
+        return this.$store.state.tagList;
+      }
     }
-  }
+})
+export default class Labels extends mixins(TagHelper) {
+ beforeCreate(){
+   this.$store.commit('fetchTags');
+ }
 }
 </script>
 
