dd032cfb9bf35498c73a3af3799c8ce5e94c02a5
wekbor <364102504@qq.com>
2020-09-16 23:47:55 +0800

    修复一进页面就弹出四个提示的bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,8 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     recordList: [],
+    createRecordError: null,
+    createTagError: null,
     tagList: [],
     currentTag: undefined
   } as RootState,
@@ -52,33 +54,36 @@ const store = new Vuex.Store({
     fetchRecords(state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
-    createRecord(state, record) {
+    createRecord(state, record:RecordItem) {
       const record2: RecordItem
         = clone(record);
       record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
-    
       store.commit('saveRecords')
-
+ 
     },
     saveRecords(state) {
       window.localStorage.setItem('recordList', JSON.stringify(state.recordList));
     },
     fetchTags(state) {
-      return state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+      if(!state.tagList || state.tagList.length === 0){
+        store.commit('createTag','衣');
+        store.commit('createTag','食');
+        store.commit('createTag','住');
+        store.commit('createTag','行');
+      }
     },
     createTag(state, name: string) {
-
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert("标签名重复");
-
+        state.createTagError = new Error('tag name duplicated');
+        return;
       }
       const id = createId().toString();
       state.tagList.push({ id, name: name });
       store.commit('saveTags');
-      window.alert("添加成功");
-
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,13 @@
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     <Tabs :data-source="recordTypeList"
         :value.sync="record.type"
-    />>
+    />
     <div class="FormItem">
-      <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
+      <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"
+      :value="record.notes"
+       />
     </div>
-    <Tags />
+    <Tags @update:value="record.tags = $event"/>
   </Layout>
 </template>
 
@@ -43,7 +45,14 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
+    if(!this.record.tags || this.record.tags.length===0){
+     return window.alert('请选择至少一个标签');
+    }
     this.$store.commit("createRecord", this.record);
+    if(this.$store.state.createRecordError === null){
+       window.alert('已保存');
+       this.record.notes ='';
+    }
   }
 }
 </script>
